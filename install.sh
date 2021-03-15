fe60798c8859458489e1c964223e175251b810be
Alexander «Azeeson» Chirikov <azeeson@gmail.com>
2021-03-15 20:55:43 +0400

    Up

--- install.sh
+++ install.sh
@@ -2,9 +2,14 @@ command_exists() {
 	command -v "$@" >/dev/null 2>&1
 }
 
-command_exists git --version || {
-    echo "Test"
+command_exists git || {
+    xcode-select --install
+}
+
+command_exists brew || {
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 }
 
 echo "Print version:"
 git --version
+brew --version
