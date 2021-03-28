8c912ce8174da4f02f25e252b866fba05b9c0dbc
Alexander «Azeeson» Chirikov <azeeson@gmail.com>
2021-03-28 22:39:59 +0400

    Up

--- start.sh
+++ start.sh
@@ -1,9 +1,42 @@
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 brew update
 brew bundle
 
 sh macos.sh
 sh extended/macos/settings.sh
 sh extended/dropbox.sh
+
+
+for app in "Activity Monitor" \
+	"Address Book" \
+	"Calendar" \
+	"cfprefsd" \
+	"Contacts" \
+	"Dock" \
+	"Finder" \
+	"Google Chrome Canary" \
+	"Google Chrome" \
+	"Mail" \
+	"Messages" \
+	"Opera" \
+	"Photos" \
+	"Safari" \
+	"SizeUp" \
+	"Spectacle" \
+	"SystemUIServer" \
+	"Terminal" \
+	"Transmission" \
+	"Tweetbot" \
+	"Twitter" \
+	"iCal"; do
+	killall "${app}" &> /dev/null
+done
+echo "Done. Note that some of these changes require a logout/restart to take effect."
 
 echo "Print version:"
 git --version
