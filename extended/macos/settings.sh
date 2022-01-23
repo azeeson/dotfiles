cc47a797973e5e422a83dd55bd5d8addeb8e3714
Alexander Medkov <azeeson@gmail.com>
2022-01-23 16:55:35 +0400

    Up

--- extended/macos/settings.sh
+++ extended/macos/settings.sh
@@ -4,6 +4,11 @@
 # Mac OS: Settings
 ###############################################################################
 
+# Set computer name (as done via System Preferences → Sharing)
+sudo scutil --set ComputerName "mbp21a"
+sudo scutil --set HostName "mbp21a"
+sudo scutil --set LocalHostName "mbp21a"
+
 # LockScreen: Set Lock Message to show on login screen
 defaults write com.apple.loginwindow LoginwindowText -string "Found me? Shoot a mail to azeeson@gmail.com to return me. Thanks!"
 
